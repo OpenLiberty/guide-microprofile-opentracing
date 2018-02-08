357ddd5ce4b69cc85ebd0297b9153671636d65f6
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2018-02-08 14:27:36 -0500

    added the section on docker

---
+++ Docker/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -0,0 +1,52 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+ // end::copyright[]
+package io.openliberty.guides.inventory;
+
+// CDI
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+// JSON-P
+import javax.json.JsonObject;
+
+// JAX-RS
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+// OpenTracing
+import org.eclipse.microprofile.opentracing.Traced;
+
+@RequestScoped
+@Path("hosts")
+public class InventoryResource {
+
+    @Inject InventoryManager manager;
+    
+    @GET
+    @Path("{hostname}")
+    @Traced(false)
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getPropertiesForHost(@PathParam("hostname") String hostname) {
+        return manager.get(hostname);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Traced(value = false)
+    public JsonObject listContents() {
+        return manager.list();
+    }
+}
