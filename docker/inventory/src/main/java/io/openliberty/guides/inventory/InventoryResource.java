846e9aadc925e13bda1200644852d0c39e8c13ce
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2018-03-02 11:02:21 -0500

    updated docker code

--- docker/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ docker/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2017, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,8 +13,6 @@
 package io.openliberty.guides.inventory;
 
 import java.util.Properties;
-
-// CDI
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -24,34 +22,34 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import io.openliberty.guides.inventory.model.InventoryList;
-
 import org.eclipse.microprofile.opentracing.Traced;
 
+import io.openliberty.guides.inventory.model.InventoryList;
+
 @RequestScoped
-@Path("systems")
+@Path("/systems")
 public class InventoryResource {
 
     @Inject InventoryManager manager;
 
     @GET
-    @Path("{hostname}")
+    @Path("/{hostname}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getPropertiesForHost(@PathParam("hostname") String hostname) {
         Properties props = manager.get(hostname);
         if (props == null) {
             return Response.status(Response.Status.NOT_FOUND)
-                                         .entity("ERROR: Unknown hostname or the resource may not be running "
-                                                 + "on the host machine")
-                                         .build();
+                           .entity("ERROR: Unknown hostname or the system service may not be running on "
+                                 + hostname)
+                           .build();
         }
         return Response.ok(props).build();
     }
 
     @GET
-    @Traced(false)
     @Produces(MediaType.APPLICATION_JSON)
     public InventoryList listContents() {
         return manager.list();
     }
+    
 }
