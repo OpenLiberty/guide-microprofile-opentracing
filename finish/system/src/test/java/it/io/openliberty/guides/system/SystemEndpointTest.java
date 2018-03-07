dee7265169794a25d02c5c34aced61145976a63e
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2018-03-07 15:31:34 -0500

    fixed package

--- finish/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointTest.java
+++ finish/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointTest.java
@@ -10,7 +10,7 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
  // end::copyright[]
-package it.io.openliberty.guides.rest;
+package it.io.openliberty.guides.system;
 
 import static org.junit.Assert.assertEquals;
 
