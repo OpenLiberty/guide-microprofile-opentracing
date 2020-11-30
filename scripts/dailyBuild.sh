16f89567c1245e5cb7c0eaed034580acb0aad9a6
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2020-11-30 15:47:49 -0500

    daily build and refactoring

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while getopts t:d:b:u: flag; do
+    case "${flag}" in
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}" ;;
+    u) DOCKER_USERNAME="${OPTARG}" ;;
+    esac
+done
+
+sed -i "\#<assemblyArtifact>#,\#</assemblyArtifact>#c<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DEVDATE"/"$DEVBUILD"</runtimeUrl></install>" pom.xml
+cat pom.xml
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
+cat Dockerfile
+
+../scripts/testApp.sh
