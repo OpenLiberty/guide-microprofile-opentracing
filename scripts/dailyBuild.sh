65836c32aabe9a9548eb5019d1dd56c3f125fc0b
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2020-12-01 11:03:24 -0500

    removed dockfile sed

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -11,7 +11,4 @@ done
 sed -i "\#<assemblyArtifact>#,\#</assemblyArtifact>#c<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DEVDATE"/"$DEVBUILD"</runtimeUrl></install>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
-cat Dockerfile
-
 ../scripts/testApp.sh
