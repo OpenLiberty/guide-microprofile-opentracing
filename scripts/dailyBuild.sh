dd215bb736a17e74b994c773b6647c7253b977bf
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2021-01-08 13:26:13 -0500

    Add daily build to GH actions (#128)
    * daily build and refactoring
    
    * changed to executable
    
    * removed dockfile sed
    
    * changed to executable
    
    * Sed fix
    
    * Fix sed command
    
    * Remove `<failOnMissingWebXml>` (see https://github.com/OpenLiberty/guides-common/issues/436)
    
    * Update test.yml
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: Austin Seto <austin.seto@ibm.com>

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while getopts t:d:b:u: flag; do
+    case "${flag}" in
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}" ;;
+    u) DOCKER_USERNAME="${OPTARG}" ;;
+    esac
+done
+
+sed -i "\#<assemblyArtifact>#,\#</assemblyArtifact>#c<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install>" pom.xml
+cat pom.xml
+
+../scripts/testApp.sh
